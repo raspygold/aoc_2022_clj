58e83f8e5dc49da5345a89f1407e6e68c4315e51
Ross Baird <483090+raspygold@users.noreply.github.com>
2022-12-04 20:32:17 +1300

    Day 04 solution

---
+++ src/aoc_2022/day04.clj
@@ -0,0 +1,39 @@
+(ns aoc-2022.day04
+  (:require [clojure.string :as str]
+            [clojure.set :refer [subset? superset? intersection]]))
+
+(defn process-input [input]
+  (->> (str/split-lines input)))
+
+(defn section-to-set [section]
+  (let [[start end] (str/split section #"-")
+        start (Long/valueOf start)
+        end (Long/valueOf end)]
+    (set (range start (+ 1 end)))))
+
+(defn sections-fully-overlap? [sections]
+  (let [[a b] (str/split sections #",")
+        a (section-to-set a)
+        b (section-to-set b)]
+    (or 
+     (subset? a b)
+     (superset? a b))))
+
+(defn sections-partially-overlap? [sections]
+  (let [[a b] (str/split sections #",")
+        a (section-to-set a)
+        b (section-to-set b)]
+    (> (count (intersection a b)) 0)))
+  
+
+(defn part1 [input]
+  (->> (process-input input)
+       (map sections-fully-overlap?)
+       (filter identity)
+       (count)))
+
+(defn part2 [input]
+  (->> (process-input input)
+       (map sections-partially-overlap?)
+       (filter identity)
+       (count)))
---
+++ test/aoc_2022/day04_test.clj
@@ -0,0 +1,17 @@
+(ns aoc-2022.day04-test
+  #_{:clj-kondo/ignore [:refer-all]}
+  (:require [clojure.test :refer :all]
+            [aoc-2022.day04 :refer :all]))
+
+(def test-input (slurp "resources/day04_test_data.txt"))
+(def puzzle-input (slurp "resources/day04_data.txt"))
+
+(deftest part1-test
+  (are [expected input] (= expected (part1 input))
+                        2 test-input
+                        573 puzzle-input))
+
+(deftest part2-test
+  (are [expected input] (= expected (part2 input))
+                        4 test-input
+                        867 puzzle-input))
